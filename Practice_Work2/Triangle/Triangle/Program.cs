e4dd65003f1c6bc5a439ef10ceab294e1581c3d5
OleksiiKliain <alexkliain35@gmail.com>
2024-03-18 19:27:31 +0200

    update

--- Practice_Work2/Triangle/Triangle/Program.cs
+++ Practice_Work2/Triangle/Triangle/Program.cs
@@ -1,12 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
-interface IPoint
-{
-    double X { get; set; }
-    double Y { get; set; }
-}
-
-class Point : IPoint
+class Point
 {
     public double X { get; set; }
     public double Y { get; set; }
@@ -25,9 +20,9 @@ interface IShape
 
 class Triangle : IShape
 {
-    private IPoint vertex1, vertex2, vertex3;
+    private Point vertex1, vertex2, vertex3;
 
-    public Triangle(IPoint vertex1, IPoint vertex2, IPoint vertex3)
+    public Triangle(Point vertex1, Point vertex2, Point vertex3)
     {
         this.vertex1 = vertex1;
         this.vertex2 = vertex2;
@@ -43,7 +38,7 @@ class Triangle : IShape
         return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
     }
 
-    private double CalculateDistance(IPoint point1, IPoint point2)
+    private double CalculateDistance(Point point1, Point point2)
     {
         return Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
     }
@@ -55,15 +50,15 @@ class Program
     {
         Console.OutputEncoding = Encoding.UTF8;
         Console.WriteLine("Введіть координати вершин трикутника:");
-        IPoint vertex1 = ReadPoint("Точка 1");
-        IPoint vertex2 = ReadPoint("Точка 2");
-        IPoint vertex3 = ReadPoint("Точка 3");
+        Point vertex1 = ReadPoint("Точка 1");
+        Point vertex2 = ReadPoint("Точка 2");
+        Point vertex3 = ReadPoint("Точка 3");
 
         IShape triangle = new Triangle(vertex1, vertex2, vertex3);
         Console.WriteLine("Площа трикутника: " + triangle.GetArea());
     }
 
-    static IPoint ReadPoint(string pointName)
+    static Point ReadPoint(string pointName)
     {
         Console.Write($"{pointName} (x): ");
         double x = double.Parse(Console.ReadLine());
