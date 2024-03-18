ce49d415aedc23cd803eb2744ce241f144c08c45
OleksiiKliain <alexkliain35@gmail.com>
2024-03-18 19:06:21 +0200

    commit
    write code

---
+++ Practice_Work2/Triangle/Triangle/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+interface IPoint
+{
+    double X { get; set; }
+    double Y { get; set; }
+}
+
+class Point : IPoint
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+interface IShape
+{
+    double GetArea();
+}
+
+class Triangle : IShape
+{
+    private IPoint vertex1, vertex2, vertex3;
+
+    public Triangle(IPoint vertex1, IPoint vertex2, IPoint vertex3)
+    {
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+        this.vertex3 = vertex3;
+    }
+
+    public double GetArea()
+    {
+        double a = CalculateDistance(vertex1, vertex2);
+        double b = CalculateDistance(vertex2, vertex3);
+        double c = CalculateDistance(vertex3, vertex1);
+        double s = (a + b + c) / 2;
+        return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    private double CalculateDistance(IPoint point1, IPoint point2)
+    {
+        return Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Введіть координати вершин трикутника:");
+        IPoint vertex1 = ReadPoint("Точка 1");
+        IPoint vertex2 = ReadPoint("Точка 2");
+        IPoint vertex3 = ReadPoint("Точка 3");
+
+        IShape triangle = new Triangle(vertex1, vertex2, vertex3);
+        Console.WriteLine("Площа трикутника: " + triangle.GetArea());
+    }
+
+    static IPoint ReadPoint(string pointName)
+    {
+        Console.Write($"{pointName} (x): ");
+        double x = double.Parse(Console.ReadLine());
+        Console.Write($"{pointName} (y): ");
+        double y = double.Parse(Console.ReadLine());
+        return new Point(x, y);
+    }
+}
